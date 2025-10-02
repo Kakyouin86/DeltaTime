9b15c61b3042e386c71b4506c88b28ec2960bb6f
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2025-10-01 21:07:26 -0300

    More updates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public CarController theCarController;
+    public CarControllerV2 theCarControllerV2;
     public Vector3 offsetDirection;
+    public float minDistance = 20f;
+    public float maxDistance = 50f;
+    public float activeDistance = 10f;
 
     void Start()
     {
@@ -12,15 +16,42 @@ public class CameraController : MonoBehaviour
         if (theCarController == null)
         {
             Debug.LogError("No CarController found in the scene.");
-            return;
         }
 
-        offsetDirection = transform.position - theCarController.transform.position;
+        if (theCarController != null)
+        {
+            offsetDirection = transform.position - theCarController.transform.position;
+        }
 
+        theCarControllerV2 = FindFirstObjectByType<CarControllerV2>();
+
+        if (theCarControllerV2 == null)
+        {
+            Debug.LogError("No CarControllerV2 found in the scene.");
+        }
+
+        if (theCarControllerV2 != null)
+        {
+            offsetDirection = transform.position - theCarControllerV2.transform.position;
+        }
+
+        activeDistance = minDistance;
+
+        offsetDirection.Normalize();
     }
 
     void Update()
     {
-        
+        if (theCarController != null)
+        {
+            activeDistance = minDistance + (maxDistance - minDistance) * (theCarController.theRB.linearVelocity.magnitude / theCarController.maxSpeed);
+            transform.position = theCarController.transform.position + (offsetDirection * activeDistance);
+        }
+
+        if (theCarControllerV2 != null) 
+        {
+            activeDistance = minDistance + (maxDistance - minDistance) * (theCarControllerV2.rb.linearVelocity.magnitude / theCarControllerV2.maxSpeed);
+            transform.position = theCarControllerV2.transform.position + (offsetDirection * activeDistance);
+        }
     }
 }
